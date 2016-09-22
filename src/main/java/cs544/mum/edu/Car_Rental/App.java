eec642cf6b988a527f85792c61c2237861aba979
deependrapathak <deependrarajpathak@gmail.com>
2016-09-22 18:09:25 -0500

    Project Created

---
+++ src/main/java/cs544/mum/edu/Car_Rental/App.java
@@ -0,0 +1,13 @@
+package cs544.mum.edu.Car_Rental;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
